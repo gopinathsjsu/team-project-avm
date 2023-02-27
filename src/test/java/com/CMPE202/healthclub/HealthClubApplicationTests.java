288d6da4977f7fd4b023bebe7a3cec90340a2622
MeeraTresa <meeratresa@gmail.com>
2023-02-27 11:30:07 -0800

    Initial Commit - exclude Data Source configuration on Spring Boot start

---
+++ src/test/java/com/CMPE202/healthclub/HealthClubApplicationTests.java
@@ -0,0 +1,19 @@
+package com.CMPE202.healthclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {HealthClubApplication.class})
+class HealthClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+
+
+
+
+}
