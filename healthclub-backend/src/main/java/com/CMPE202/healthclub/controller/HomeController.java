24457b1040f159103a1b8c289c27576f71c1fa88
MeeraTresa <meeratresa@gmail.com>
2023-03-01 09:00:25 -0800

    Adding home controller

---
+++ healthclub-backend/src/main/java/com/CMPE202/healthclub/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.CMPE202.healthclub.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    public HomeController() {
+    }
+    @GetMapping({"/home"})
+    public String home() {
+        return "Hello Meera";
+    }
+}
