575b72df12c91d858dbec4b43599d2431f9c21d6
MeeraTresa <meeratresa@gmail.com>
2023-03-06 13:04:01 -0800

    Making home API accessible to all /api/v1/home

--- healthclub-backend/src/main/java/com/CMPE202/healthclub/controller/HomeController.java
+++ healthclub-backend/src/main/java/com/CMPE202/healthclub/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     public HomeController() {
     }
-    @GetMapping({"/"})
+    @GetMapping({"/home"})
     public String home() {
         return "<h1>Welcome to Health Club</h1>";
     }
