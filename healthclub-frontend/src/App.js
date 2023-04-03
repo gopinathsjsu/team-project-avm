4f6330fd7a38aaf49bf9be6429cb24455acb0695
Adesh Landge <37284267+adeshlandge@users.noreply.github.com>
2023-04-03 15:49:39 -0700

    Added changes to build Home page components.
    Added changes to build Home page components.

--- healthclub-frontend/src/App.js
+++ healthclub-frontend/src/App.js
@@ -1,47 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import vid from './res/trainingVideo.mp4';
-import { FaUserAlt } from "react-icons/fa";
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import NavbarComponent from "./components/NavbarComponent.js";
+import Membership from "./components/Membership.js";
+import Locations from "./components/Locations.js";
+import Workout from './components/Workouts';
 
 function App() {
-
-
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <header>
-        <div class="header">
-          <nav class="navigation">
-            <a href="#" class="navbar-logo">HealthClub</a>
-            <div class="navbar-right">
-              <a href="#">The Workout</a>
-              <a href="#">Locations</a>
-              <a href="#">Joining</a>
-              <a href="#"><FaUserAlt size={17}/></a>
-            </div>
-          </nav>
-          <div class="video-container">
-            <video autoPlay loop muted src={vid} id='myVideo'>
-              {/* <source  type='video/mp4'></source> */}
-            </video>
-          </div>
-        </div>
-      </header>
+    <div>
+      <Router>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/workout" element={<Workout />} />
+          <Route path="/memberships" exact element={<Membership />} />
+          <Route path="/locations" exact element={<Locations />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
