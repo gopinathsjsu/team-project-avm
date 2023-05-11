9c2cd815a59a79045065ed9be60ce1f2bc7e21c3
VineethHamilpur590 <115372818+VineethHamilpur590@users.noreply.github.com>
2023-05-11 14:31:36 -0700

    Adding Authentication context for Member as well

---
+++ healthclub-frontend/src/context/MemberAuth.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const MemberAuth = (Component) => {
+    class MemberAuthenticatedComponent extends React.Component {
+        render() {
+            let isAuthenticated = false;
+            const storedObject = JSON.parse(window.sessionStorage.getItem("USER_DETAILS"));
+            console.log("loggedin user data==>", storedObject);
+            if (storedObject) {
+                if (storedObject.token && (storedObject.role === 'MEMBER' || storedObject.role === 'FREE_TRIAL_MEMBER')) {
+                    isAuthenticated = true;
+                }
+            }
+
+            if (!isAuthenticated) {
+                return <Navigate to="/" />;
+            }
+
+            return <Component />;
+        }
+
+    }
+    return MemberAuthenticatedComponent;
+};
+
+export default MemberAuth;
